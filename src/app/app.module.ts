43362cdd1820d8defc69b8c520b49c19fdd7fe4b
rufomaryann <shajeemaru@gmail.com>
2019-07-07 00:00:50 +0800

    Import and configuration of client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CourseComponent } from './components/course/course.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
+
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, 
+            MatToolbarModule, MatSelectModule, MatFormFieldModule, MatTableModule, 
+            MatPaginatorModule, MatSortModule, MatProgressBarModule, MatIconModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule  } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    CourseComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressBarModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
